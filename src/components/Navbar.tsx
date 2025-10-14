1705671f5b824d023ca73d4e66c90b73925d6943
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-10-14 10:42:08 +0300

    test with working vite config with css injection in js file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,50 @@
-import {Button, ButtonEnum} from 'ui-design-system';
-import "ui-design-system/dist/ui-design-system.css";
-
+import { Button, ButtonEnum } from "ui-design-system";
 
 function Navbar() {
+  const handleClick = () => {
+    console.log("I was clicked!");
+  };
 
-    const handleClick = () => {
-        console.log('I was clicked!');
-    };
-
-    return <div className='Navbar'>
-        <Button variant={ButtonEnum.Primary} clickHandler={handleClick}>Primary Button</Button>
-        <Button variant={ButtonEnum.Secondary} disabled={true} clickHandler={handleClick}>Primary Button</Button>
-        <Button variant={ButtonEnum.Underline} loading={true} loader={{type: 'spinner', mainColor: 'blue', subColor: 'red', size: 20}} clickHandler={handleClick}>Primary Button</Button>
-        <Button variant={ButtonEnum.Underline} loading={true} loader={{type: 'dots', size: 10, mainColor: 'red', subColor: 'yellow'}} clickHandler={handleClick}>Primary Button</Button>
+  return (
+    <div className="Navbar">
+      <Button variant={ButtonEnum.Primary} clickHandler={handleClick}>
+        Primary Button
+      </Button>
+      <Button
+        variant={ButtonEnum.Secondary}
+        disabled={true}
+        clickHandler={handleClick}
+      >
+        Primary Button
+      </Button>
+      <Button
+        variant={ButtonEnum.Underline}
+        loading={true}
+        loader={{
+          type: "spinner",
+          mainColor: "blue",
+          subColor: "red",
+          size: 20,
+        }}
+        clickHandler={handleClick}
+      >
+        Primary Button
+      </Button>
+      <Button
+        variant={ButtonEnum.Underline}
+        loading={true}
+        loader={{
+          type: "dots",
+          size: 10,
+          mainColor: "red",
+          subColor: "yellow",
+        }}
+        clickHandler={handleClick}
+      >
+        Primary Button
+      </Button>
     </div>
+  );
 }
 
 export default Navbar;
